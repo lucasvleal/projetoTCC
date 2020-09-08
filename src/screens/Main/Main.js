f5c8ae3f29a30630a528f4ca1f2dc966a75b66ae
Lucas Leal <lucasvianil@gmail.com>
2020-09-08 17:54:20 -0300

    Add modal of select object

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, StatusBar, TouchableOpacity } from 'react-native';
+import { View, StatusBar, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
+import Modal from 'react-native-modal';
 
 import styles from './Style';
 import { BoldText, RegularText, LightText } from '../../components/StyledText';
@@ -14,6 +15,8 @@ import api from './../../services/Api';
 
 export default function Main() {
     const [photo, setPhoto] = useState("");
+    const [productSelected, setProductSelected] = useState(null);
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
     // useEffect(() => console.log("image: ", photo), [photo]);
 
@@ -22,7 +25,7 @@ export default function Main() {
         let result;
 
         if (!granted) {
-            resp = await ImagePicker.requestCameraRollPermissionsAsync();
+            const resp = await ImagePicker.requestCameraRollPermissionsAsync();
 
             if (!resp.granted) {
                 return alert("Permissão à galeria negada!");
@@ -48,7 +51,7 @@ export default function Main() {
         let result;
 
         if (!granted) {
-            resp = await ImagePicker.requestCameraPermissionsAsync();
+            const resp = await ImagePicker.requestCameraPermissionsAsync();
 
             if (!resp.granted) {
                 return alert("Permissão à câmera negada!");
@@ -77,41 +80,146 @@ export default function Main() {
             return alert("Sem nenhuma foto para a requisição!");
         }
 
-        try {
-            const data = new FormData();
-            data.append('image', { 
-                uri: photo.url, 
-                name: 'first-photo.png', 
-                filename :'firstImageName.png', 
-                type: photo.type
-            });
-            data.append('Content-Type', photo.type);    
-            console.log("data:", data);
+        toggleModal();
+
+        // try {
+        //     const data = new FormData();
+
+        //     data.append('image', { 
+        //         uri: photo.url, 
+        //         name: 'first-photo.png', 
+        //         filename :'firstImageName.png', 
+        //         type: photo.type
+        //     });
+        //     data.append('Content-Type', photo.type);    
+        //     console.log("data:", data);
     
-            await api.post('/images/', data, { headers: { "Content-type": "multipart/form-data" }})
-                .then((data) => {
-                    console.log("Passou and data from api: ", data.data);
+        //     await api.post('/images/', data, { headers: { "Content-type": "multipart/form-data" }})
+        //         .then((data) => {
+        //             console.log("Passou and data from api: ", data.data);
             
-                    if (data.status === 201) {
-                        alert("Imagem enviada com sucesso!");
-                    } else {
-                        alert("Erro no envio da imagem.");
-                    }
-                })
-                .catch(err => {
-                    console.log("Erro:", err);
-                });
+        //             if (data.status === 201) {
+        //                 alert("Imagem enviada com sucesso!");
+        //             } else {
+        //                 alert("Erro no envio da imagem.");
+        //             }
+        //         })
+        //         .catch(err => {
+        //             console.log("Erro:", err);
+        //         });
             
-        } catch(err) {
-            console.log("ERROOOOW:", err);
-            alert("Ocorreu um erro");
-        }        
+        // } catch(err) {
+        //     console.log("ERROOOOW:", err);
+        //     alert("Ocorreu um erro");
+        // }        
     } 
+
+    function handleSelectProduct(id) {
+        if (productSelected === id) {
+            setProductSelected(null);
+        } else {
+            setProductSelected(id);
+        }
+    }
+
+    function toggleModal() {
+        setIsModalVisible(!isModalVisible);
+
+        if (!isModalVisible) {
+            setProductSelected(null);
+        }
+    }
     
 
   return (
     <>
         <StatusBar barStyle="dark-content" />
+
+        <Modal
+            isVisible={isModalVisible}
+            style={styles.modal}
+            onBackButtonPress={() => setIsModalVisible(false)}
+            onBackdropPress={() => setIsModalVisible(false)}
+        >
+            <View style={{ flex: 1 }}>
+                <View style={styles.boxTitleModal}>
+                    <BoldText style={styles.titleModal}>Escolha o objeto desejado</BoldText>
+                </View>
+
+                <View style={styles.boxContentModal}>
+                    <ScrollView
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        contentContainerStyle={{ paddingHorizontal: 10 }}
+                    >
+                        <TouchableOpacity 
+                            onPress={() => handleSelectProduct(1)}
+                            style={{ 
+                                borderWidth: productSelected === 1 ? 5 : 0,
+                                borderColor: productSelected === 1 ? Colors.pcTransp : 'transparent',
+                                marginHorizontal: 10,
+                                borderRadius: 15, 
+                            }}
+                        >
+                            <Image source={{ uri: photo.url }} style={styles.imageModal} />
+                        </TouchableOpacity>
+
+                        <TouchableOpacity 
+                            onPress={() => handleSelectProduct(2)}
+                            style={{ 
+                                borderWidth: productSelected === 2 ? 5 : 0,
+                                borderColor: productSelected === 2 ? Colors.pcTransp : 'transparent',
+                                marginHorizontal: 10,
+                                borderRadius: 15, 
+                            }}
+                        >
+                            <Image source={{ uri: photo.url }} style={styles.imageModal} />
+                        </TouchableOpacity>
+
+                        <TouchableOpacity 
+                            onPress={() => handleSelectProduct(3)}
+                            style={{ 
+                                borderWidth: productSelected === 3 ? 5 : 0,
+                                borderColor: productSelected === 3 ? Colors.pcTransp : 'transparent',
+                                marginHorizontal: 10,
+                                borderRadius: 15, 
+                            }}
+                        >
+                            <Image source={{ uri: photo.url }} style={styles.imageModal} />
+                        </TouchableOpacity>
+
+                        <TouchableOpacity 
+                            onPress={() => handleSelectProduct(4)}
+                            style={{ 
+                                borderWidth: productSelected === 4 ? 5 : 0,
+                                borderColor: productSelected === 4 ? Colors.pcTransp : 'transparent',
+                                marginHorizontal: 10,
+                                borderRadius: 15, 
+                            }}
+                        >
+                            <Image source={{ uri: photo.url }} style={styles.imageModal} />
+                        </TouchableOpacity>
+                    </ScrollView>
+                </View>
+
+                <View style={styles.boxFooterModal}>
+                    <TouchableOpacity 
+                        disabled={productSelected ? false : true} 
+                        style={[styles.btnSearch, { opacity: productSelected ? 1 : 0.3 }]}
+                        onPress={toggleModal}
+                    >
+                        <BoldText style={{ opacity: productSelected ? 1 : 0.3 }}>Pesquisar</BoldText>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity 
+                        style={styles.btnCloseModal}
+                        onPress={toggleModal}
+                    >
+                        <BoldText style={{ color: 'tomato' }}>X</BoldText>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </Modal>
         
         <View style={globalStyles.container}>
             <View style={styles.containerHeader}>
--- src/screens/Main/Style.js
+++ src/screens/Main/Style.js
@@ -3,6 +3,86 @@ import { StyleSheet } from 'react-native';
 import Colors from '../../constants/Colors';
 
 const styles = StyleSheet.create({ 
+    modal: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        // padding: 10,
+    },
+
+    boxTitleModal: {
+        flex: 0.2,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        // backgroundColor: 'blue',
+    },
+
+        titleModal: {
+            fontSize: 22,
+            color: Colors.pc,
+            textAlign: 'center',
+        },
+
+    boxContentModal: {
+        flex: 0.6,
+        // justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+
+
+        // backgroundColor: 'yellow',
+    },
+
+        imageModal: {
+            width: 220,
+            height: 220,
+            // marginHorizontal: 10,
+            borderRadius: 10,
+        },
+
+    boxFooterModal: {
+        flex: 0.2,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        // backgroundColor: 'green',
+    },
+
+        btnSearch: {
+            width: '80%',
+            height: '50%',
+            borderRadius: 40,
+
+            justifyContent: 'center',
+            alignItems: 'center',
+
+            backgroundColor: Colors.sc,
+
+            shadowColor: Colors.pcTransp,
+            shadowOpacity: 0.25,
+            shadowRadius: 2.5,
+            shadowOffset: {
+                height: 2,
+                width: 0.5
+            },
+            elevation: 4,
+        },
+
+        btnCloseModal: {
+            width: 30,
+            height: 30,
+            marginVertical: 10,
+
+            borderRadius: 15,
+            borderColor: 'tomato',
+            borderWidth: 1,
+
+            justifyContent: 'center',
+            alignItems: 'center',
+
+            backgroundColor: 'transparent',            
+        },
+
     containerHeader: {
         flex: 0.1,
 
@@ -94,8 +174,7 @@ const styles = StyleSheet.create({
         txtFooter: {
             color: Colors.pcTransp,
             fontSize: 12,
-        },
-    
+        },   
   });
   
 export default styles;
